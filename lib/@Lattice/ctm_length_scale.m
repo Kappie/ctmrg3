1cde81abe5ed530ed34f8ad6ca55b83b3b28a9a4
Geert Kapteijns <ghkapteijns@gmail.com>
2017-02-23 10:36:05 +0100

    ctm length scale

---
+++ lib/@Lattice/ctm_length_scale.m
@@ -0,0 +1,8 @@
+function value = ctm_length_scale(obj, C, T)
+  eigenvalues = diag(C);
+  % make sure Tr(C^4) = 1.
+  eigenvalues = Util.scale_by_trace_condition(eigenvalues);
+  % make sure eigenvalues sorted from large to small
+  eigenvalues = sort(eigenvalues, 'descend');
+  value = exp(1 / log(eigenvalues(1)/eigenvalues(2)));
+end
