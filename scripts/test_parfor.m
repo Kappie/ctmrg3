8158225c74c4354e89ebf9e7020ac1fb7a6a7f88
Kappie <ghkapteijns@gmail.com>
2017-02-24 18:59:15 +0100

    lol

--- scripts/test_parfor.m
+++ scripts/test_parfor.m
@@ -1,17 +1,24 @@
 function test_parfor
   number_of_threads = 10;
-  matrix_size = 2500;
+  matrix_size = 3000;
 
   profile on
   rng(1)
+  parfor_svd(number_of_threads, matrix_size);
+  rng(1)
+  for_svd(number_of_threads, matrix_size);
+  profile off
+  profsave(profile('info'), 'profile_results_parfor')
+end
+
+function parfor_svd(number_of_threads, matrix_size)
   parfor i = 1:number_of_threads
     eig(rand(matrix_size));
   end
+end
 
-  rng(1)
-  for i = 1:number_of_threads
+function for_svd(number_of_times, matrix_size)
+  for i = 1:number_of_times
     eig(rand(matrix_size));
   end
-  profile off
-  profsave(profile('info'), 'profile_results_parfor')
 end
