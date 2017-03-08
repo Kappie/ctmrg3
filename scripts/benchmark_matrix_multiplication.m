00e16cb8b5f3c6ba061c6c1e04a6244d5e970aa1
Kappie <ghkapteijns@gmail.com>
2017-03-08 10:54:15 +0100

    timeit results

--- scripts/benchmark_matrix_multiplication.m
+++ scripts/benchmark_matrix_multiplication.m
@@ -1,13 +1,19 @@
 function benchmark_matrix_multiplication
   rng(1)
-  sizes = [20 50 100 200 500 800 1000 1250 1500 2000 3000];
-  timeits = zeros(1, numel(sizes));
+  maxNumCompThreads(1)
+  sizes = [20 50 100 300 500 700 900 1200 1500 1800 2100 2600 3100];
+  timeits_matrix_mult = zeros(1, numel(sizes));
+  timeits_svd = zeros(1, numel(sizes));
+
   for i = 1:numel(sizes)
     A = rand(sizes(i));
     B = rand(sizes(i));
-    f = @() A * B;
-    timeits(i) = timeit(f);
+    multiplication = @() A * B;
+    perform_svd = @() svd(A);
+    timeits_matrix_mult(i) = timeit(multiplication);
+    timeits_svd(i) = timeit(perform_svd);
   end
 
-  markerplot(sizes, timeits, '--', 'loglog')
+  save('benchmark_mult_svd_singlethread.mat', 'sizes', 'timeits_matrix_mult', 'timeits_svd');
+
 end
