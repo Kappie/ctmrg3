9f959d608ec084f26dee410941bbf3b331596b46
Kappie <ghkapteijns@gmail.com>
2017-03-16 10:58:08 +0100

    set data collapse q = 4 N

--- scripts/test_data_collapse_N.m
+++ scripts/test_data_collapse_N.m
@@ -2,7 +2,7 @@ function test_data_collapse_N
   q = 4;
   truncation_error = 1e-5;
   % N_values = [20 80 160 320 480 600 1000 1500];
-  N_values = [20 60 100 140 180];
+  N_values = [20 60 100 120 140 160 180 200];
   width = 0.1; number_of_points = 10;
   temperatures = linspace(Constants.T_crit_guess(q) - width, ...
     Constants.T_crit_guess(q) + width, number_of_points);
@@ -10,6 +10,8 @@ function test_data_collapse_N
     Constants.T_crit_guess(q) + width/10, number_of_points);
   temperatures_super_zoom = linspace(Constants.T_crit_guess(q) - width/100, ...
     Constants.T_crit_guess(q) + width/100, 5);
+
+  temperatures = sort([temperatures temperatures_zoom temperatures_super_zoom])
 
   collapse = DataCollapseN(q, N_values, temperatures, truncation_error)
   collapse = collapse.find_best_collapse();
