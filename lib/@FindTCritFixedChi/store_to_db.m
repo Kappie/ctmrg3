2a0c02093d4b006e5107a70861ac73e922e5ef8c
Geert Kapteijns <ghkapteijns@gmail.com>
2017-05-15 11:35:09 +0200

    fix bug in storing t pseudocrit to db

--- lib/@FindTCritFixedChi/store_to_db.m
+++ lib/@FindTCritFixedChi/store_to_db.m
@@ -1,6 +1,6 @@
 function store_to_db(obj, T_pseudocrit, q, chi, tensors)
   query = ['insert into t_pseudocrits (t_pseudocrit, q, chi, c, t, tol_x, tolerance, method) ' ...
-    'values (?, ?, ?, ?, ?, ?)'];
+    'values (?, ?, ?, ?, ?, ?, ?, ?)'];
   [bytesC, bytesT] = Util.serialize_tensors(tensors.C, tensors.T);
 
   sqlite3.execute(obj.db_id, query, T_pseudocrit, q, chi, bytesC, bytesT, obj.TolX, obj.tolerance, obj.method);
