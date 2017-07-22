36da73f8b5e44f997a600fb8fd15d5eac58e9729
Geert Kapteijns <ghkapteijns@gmail.com>
2017-07-22 11:06:04 +0200

    set values simulations_clock_model q 6 chi 10-80 tol 1e-7 zoom

--- scripts/simulations_clock_model.m
+++ scripts/simulations_clock_model.m
@@ -1,18 +1,20 @@
 function simulations_clock_model
   q = 6;
-  % number_of_points = 10;
+  number_of_points = 10;
   % number_of_points = 5;
   % left_bound_T1 = 0.9; right_bound_T1 = 0.92;
   % left_bound_T2 = 0.93; right_bound_T2 = 0.95;
   % left_bound_T1 = 0.89; right_bound_T1 = 0.90;
   % left_bound_T2 = 0.95; right_bound_T2 = 0.96;
-  % temperatures_zoom_T1 = linspace(left_bound_T1, right_bound_T1, number_of_points);
-  % temperatures_zoom_T2 = linspace(left_bound_T2, right_bound_T2, number_of_points);
-  % temperatures = [temperatures_zoom_T1 temperatures_zoom_T2];
+  left_bound_T1 = 0.69; right_bound_T1 = 0.71;
+  left_bound_T2 = 0.88; right_bound_T2 = 0.92;
+  temperatures_zoom_T1 = linspace(left_bound_T1, right_bound_T1, number_of_points);
+  temperatures_zoom_T2 = linspace(left_bound_T2, right_bound_T2, number_of_points);
+  temperatures = [temperatures_zoom_T1 temperatures_zoom_T2];
   % number_of_points = 25; left_bound = 0.80; right_bound = 1.05;
-  number_of_points = 50; left_bound = 0.6; right_bound = 1.1;
+  % number_of_points = 50; left_bound = 0.6; right_bound = 1.1;
 
-  temperatures = linspace(left_bound, right_bound, number_of_points);
+  % temperatures = linspace(left_bound, right_bound, number_of_points);
   % chi_values = 10:10:120;
   % chi_values = 10:10:100;
   chi_values = 10:10:80;
@@ -22,7 +24,7 @@ function simulations_clock_model
   tolerances = [8e-7 4e-7 2e-7 1e-7];
 
   sim = FixedToleranceSimulation(temperatures, chi_values, tolerances, q).run();
-  save('q6_chi10-80_tol1e-7.mat', 'sim')
+  save('q6_chi10-80_tol1e-7_zoom.mat', 'sim')
   % order_params = sim.compute('order_parameter');
   % entropies = sim.compute('entropy');
   % corr_lengths = sim.compute('correlation_length')
